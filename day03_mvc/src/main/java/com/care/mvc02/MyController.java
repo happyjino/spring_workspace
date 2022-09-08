5aea8f88a99b0db9df36d69f49fda7ff73edfbb4
happyjino <wlsgh1128@naver.com>
2022-09-08 13:25:59 +0900

    4일차 - mvc02
    get, post 방식으로 정보를 받고 출력하기~

--- day03_mvc/src/main/java/com/care/mvc02/MyController.java
+++ day03_mvc/src/main/java/com/care/mvc02/MyController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("mc")
 // mvc 패키지의 mycontroller와 이름이 겹치므로 어노테이션 옆에 괄호 안에 이름을 붙여줄 수 있다!
@@ -16,9 +18,32 @@ public class MyController {
 		return "get_post/index";
 	}
 	@GetMapping("my/result")
-	public void result01(HttpServletRequest request, Model model){
+	public String result01(HttpServletRequest request, Model model){
 		System.out.println("method : " +  request.getMethod());
 		System.out.println("name : " + request.getParameter("name"));
 		System.out.println("age : " + request.getParameter("age"));
+		
+		model.addAttribute("method", request.getMethod());
+		model.addAttribute("name", request.getParameter("name"));
+		model.addAttribute("age", request.getParameter("age"));
+		
+		return "get_post/result";
 	}
+	@PostMapping("my/result")
+	public String result01(
+			@RequestParam String name,
+			@RequestParam("age") int a,
+			HttpServletRequest request, 
+			Model model){
+		System.out.println("method : " +  request.getMethod());
+		System.out.println("name : " + request.getParameter("name"));
+		System.out.println("age : " + request.getParameter("age"));
+		
+		model.addAttribute("method", request.getMethod());
+		model.addAttribute("name", name);
+		model.addAttribute("age", a);
+		
+		return "get_post/result";
+	}
+	
 }
---
+++ day03_mvc/src/main/webapp/WEB-INF/views/get_post/result.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>result.jsp<br>
+	<h3>${method } 방식</h3>
+	이름 : ${name }<br>
+	나이 : ${age }<br>
+
+</body>
+</html>
