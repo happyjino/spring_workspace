41adf13ad188b6cf156199f60714eb7674a9aa5c
happyjino <wlsgh1128@naver.com>
2022-09-12 13:45:16 +0900

    4일차 - 퀴즈 해답10
    dao에 register 함수를 만들어 주고
    아직 데이터베이스를 따로 사용하지 않으므로
    배열에 저장해준다.

--- day04_mvc_answer/src/main/java/com/care/member/dao/MemberDAO.java
+++ day04_mvc_answer/src/main/java/com/care/member/dao/MemberDAO.java
@@ -1,8 +1,20 @@
 package com.care.member.dao;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Repository;
+
+import com.care.member.dto.MemberDTO;
 
 @Repository
 public class MemberDAO {
-
+	ArrayList<MemberDTO> DB;
+	
+	public MemberDAO() {
+		DB = new ArrayList<MemberDTO>();
+	}
+	
+	public void register(MemberDTO dto) {
+		DB.add(dto);
+	}
 }
