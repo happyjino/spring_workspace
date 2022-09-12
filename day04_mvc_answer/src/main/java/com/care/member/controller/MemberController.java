abac43d29efe673f2b5078098f6c827ad1156314
happyjino <wlsgh1128@naver.com>
2022-09-12 13:36:35 +0900

    4일차 - 퀴즈 해답8
    register_form에서 데이터가 잘 넘어오는지 확인하기!
    MemberDTO dto를 매개 변수로 받고
    dto.getId()와 같은 getter 함수로 확인 가능
    Autowired 어노테이션을 이용해 MemberService와 연결해주고
    객체가 잘 넘어오는지 확인해보기

--- day04_mvc_answer/src/main/java/com/care/member/controller/MemberController.java
+++ day04_mvc_answer/src/main/java/com/care/member/controller/MemberController.java
@@ -1,11 +1,17 @@
 package com.care.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.care.member.dto.MemberDTO;
+import com.care.member.service.MemberService;
+
 @Controller
 public class MemberController {
+	@Autowired MemberService ms;
+	
 	@GetMapping("index")
 	public String index() {
 		return "index";
@@ -15,8 +21,14 @@ public class MemberController {
 		return "register_form";
 	}
 	@PostMapping("register")
-	public String register() {
+	public String register(MemberDTO dto) {
+		// System.out.println(dto.getId());
+		// System.out.println(dto.getPw());
+		// System.out.println(dto.getName());
 		
+		System.out.println("ms : " + ms);
+		// 이건 객체가 들어오는지 확인하는것!!
+		// 이렇게 꼭 확인해주기
 		return null;
 	}
 	
