dcb434fa959fe7efe8d8e8de99d1ccd15c0d9ccf
happyjino <wlsgh1128@naver.com>
2022-09-12 13:31:48 +0900

    4일차 - 퀴즈 해답7
    regitster에 대한 컨트롤을 위해서
    register_form에서 받아오는 변수들을 DTO에도 만들고
    getter, setter 설정하기

--- day04_mvc_answer/src/main/java/com/care/member/controller/MemberController.java
+++ day04_mvc_answer/src/main/java/com/care/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.care.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,6 +13,11 @@ public class MemberController {
 	@GetMapping("register_form")
 	public String register_form() {
 		return "register_form";
+	}
+	@PostMapping("register")
+	public String register() {
+		
+		return null;
 	}
 	
 }
--- day04_mvc_answer/src/main/java/com/care/member/dto/MemberDTO.java
+++ day04_mvc_answer/src/main/java/com/care/member/dto/MemberDTO.java
@@ -1,5 +1,31 @@
 package com.care.member.dto;
 
 public class MemberDTO {
+	private String id, pw, name;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
