d5fdfd1639396c858cede6c1f4b0e8f8cebbf841
happyjino <wlsgh1128@naver.com>
2022-09-13 12:39:57 +0900

    5일차 - 준비
    새 프로젝트 만들고 pom.xml, web.xml 수정 완료

--- day04_mvc_quiz/src/main/java/com/care/member/service/MemberService.java
+++ day04_mvc_quiz/src/main/java/com/care/member/service/MemberService.java
@@ -3,6 +3,8 @@ package com.care.member.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.care.member.dao.MemberDAO;
+
 @Service
 public class MemberService {
 	@Autowired MemberDAO dao;
