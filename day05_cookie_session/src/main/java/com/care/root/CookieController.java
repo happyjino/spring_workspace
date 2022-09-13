f16645bed3c62d9b6bc1ee7ffaa38f16cfb1f611
happyjino <wlsgh1128@naver.com>
2022-09-13 14:01:19 +0900

    5일차 - 쿠키 넘겨주고 불러오기1
    response에 쿠키를 넣는거 하고
    request를 이용해서 쿠키를 불러오는 것을 해봤다.
    옛날 방식이므로
    @CookieValue 방식을 이용해 쿠키를 불러오는 것을 해보고
    에러 처리를 하는 것도 배웠다.required = false

--- day05_cookie_session/src/main/java/com/care/root/CookieController.java
+++ day05_cookie_session/src/main/java/com/care/root/CookieController.java
@@ -1,12 +1,34 @@
 package com.care.root;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CookieController {
 	@GetMapping("cookie")
-	public String myCookie() {
+	public String myCookie(
+			HttpServletResponse response, 
+			HttpServletRequest request,
+			@CookieValue(value="myCookie", required=false ) Cookie cook ) {
+		System.out.println("cook : " + cook);
+		/*
+		Cookie[] cooks = request.getCookies();
+		for(Cookie c : cooks) {
+			System.out.println(c.getName());
+			System.out.println(c.getValue());
+		} 예전 방식들~~~
+		*/
+		
+		Cookie cookie = new Cookie("myCookie", "testCookie");
+		cookie.setMaxAge(5);
+		cookie.setPath("/");
+		
+		response.addCookie(cookie);
 		return "cookie/cookie";
 	}
 }
