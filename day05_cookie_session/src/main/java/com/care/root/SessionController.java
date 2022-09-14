9b6df2acbd6e8bff2b89aa27be3ac7e3d8774776
happyjino <wlsgh1128@naver.com>
2022-09-14 12:31:32 +0900

    6일차 - 로그인 구현을 위한 세팅
    로그인 구현을 위해 login, logout, main을 jsp 파일로 만들고
    login에 대한 폼과 컨트롤을 만들어준다.

--- day05_cookie_session/src/main/java/com/care/root/SessionController.java
+++ day05_cookie_session/src/main/java/com/care/root/SessionController.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SessionController {
@@ -29,5 +31,16 @@ public class SessionController {
 		session.removeAttribute("id"); // 특정 세션 만료
 		// session.invalidate(); // 모든 세션 만료
 		return "session/delSession";
+	}
+	@GetMapping("login")
+	public String login() {
+		return "session/login";
+	}
+	@PostMapping("chk_user")
+	public String chkUser(
+			@RequestParam String id,
+			@RequestParam String pw,
+			HttpSession session) {
+		return null;
 	}
 }
---
+++ day05_cookie_session/src/main/webapp/WEB-INF/views/session/login.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>login.jsp<br>
+
+<form action="chk_user" method="post">
+	<input type="text" name="id" placeholder="input id"><br>
+	<input type="password" name="pw" placeholder="input password"><br>
+	<input type="submit" value="로그인"><br>
+</form>
+
+</body>
+</html>
