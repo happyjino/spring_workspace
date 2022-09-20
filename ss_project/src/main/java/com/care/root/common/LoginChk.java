7277b063b5eba62ce078782146cad59e2bef0df7
happyjino <wlsgh1128@naver.com>
2022-09-20 14:12:39 +0900

    11일차 - 로그인체크 인터셉트

--- ss_project/src/main/java/com/care/root/common/LoginChk.java
+++ ss_project/src/main/java/com/care/root/common/LoginChk.java
@@ -1,5 +1,7 @@
 package com.care.root.common;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -13,7 +15,12 @@ public class LoginChk extends HandlerInterceptorAdapter{
 			throws Exception {
 		HttpSession session = request.getSession();
 		if(session.getAttribute("login") == null) {
-			response.sendRedirect("member/login");
+			//response.sendRedirect("login");
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			//out.print("<h1>로그인 진행하세요</h1>");
+			out.print("<script>alert('로그인 진행하세요');location.href='/root/member/login'</script>");
+			
 			return false;
 		}
 		return true;
