578066aa9940beca40197f7acdf8c6afac77e9e0
happyjino <wlsgh1128@naver.com>
2022-09-24 01:02:44 +0900

    13일차 - 서비스에서 파일 전송하기!
    특정 위치에 파일 저장하기 성공~

--- file/src/main/java/com/care/root/controller/FileController.java
+++ file/src/main/java/com/care/root/controller/FileController.java
@@ -1,24 +1,38 @@
 package com.care.root.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.care.root.service.FileServiceImpl;
 
 @Controller
 public class FileController {
+	@Autowired FileServiceImpl fs;
 	
 	@GetMapping("form")
 	public String form() {
 		return "uploadForm";
 	}
 	
-	@PostMapping("upload")
+	@PostMapping("upload2")
 	public void upload(@RequestParam String id, @RequestParam String name, @RequestParam MultipartFile file) {
 		System.out.println(id);
 		System.out.println(name);
 		System.out.println(file.getOriginalFilename());
+	}
+	@PostMapping("upload")
+	public String upload2(MultipartHttpServletRequest mul) {
+		String id = mul.getParameter("id");
+		String name = mul.getParameter("name");
+		MultipartFile f = mul.getFile("file");
+		fs.fileProcess(mul);
+		
+		return "redirect:form";
 	}
 
 }
--- file/src/main/java/com/care/root/service/FileServiceImpl.java
+++ file/src/main/java/com/care/root/service/FileServiceImpl.java
@@ -1,5 +1,28 @@
 package com.care.root.service;
 
-public class FileServiceImpl {
+import java.io.File;
+import java.io.IOException;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+@Service
+public class FileServiceImpl {
+	public static String IMAGE_REPO;
+	static {
+		IMAGE_REPO = "C:/spring/image_repo/";
+	}
+	public void fileProcess(MultipartHttpServletRequest mul) {
+		MultipartFile file = mul.getFile("file");
+		if(file.getSize() != 0) {
+			File saveFile = new File(IMAGE_REPO+file.getOriginalFilename());
+			try {
+				file.transferTo(saveFile);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
 }
